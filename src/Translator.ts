f81c88835d3e6597c896c13e894e3f3176f4f41e
Peter Ullrich <p.ullrich@dotup.de>
2019-03-02 19:27:53 +0100

    Translator<T extends string>

--- src/Translator.ts
+++ src/Translator.ts
@@ -1,7 +1,8 @@
 import { FormatOptions } from './FormatOptions';
 
+// tslint:disable:no-any
 // tslint:disable-next-line: interface-name
-export interface Translator<T> {
+export interface Translator<T extends string> {
   // format(options: FormatOptions<T>, ...args: any[]): string;
   format(options: T | FormatOptions<T>, ...args: any[]): string;
   getText(key: T, ...args: any[]): string;
